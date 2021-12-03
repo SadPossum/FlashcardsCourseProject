4bcf0e34e5dbd5864f435d0c4096975eddaf60b9
stillProger <killumexxx@gmail.com>
2021-12-03 15:53:28 +0400

    feat: new views

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/ViewModels/CardSetViewModel.cs
@@ -0,0 +1,85 @@
+﻿using KursahProject.Models;
+using KursahProject.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace KursahProject.ViewModels
+{
+    public class CardSetViewModel : BaseViewModel
+    {
+        private CardSet _selectedItem;
+
+        public ObservableCollection<CardSet> Items { get; }
+        public Command LoadItemsCommand { get; }
+        public Command AddItemCommand { get; }
+        public Command<CardSet> CardSetTapped { get; }
+
+        public CardSetViewModel()
+        {
+            Title = "Наборы карточек";
+            Items = new ObservableCollection<CardSet>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            CardSetTapped = new Command<CardSet>(OnItemSelected);
+
+            AddItemCommand = new Command(OnAddItem);
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = await CardSetDataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public void OnAppearing()
+        {
+            IsBusy = true;
+            SelectedItem = null;
+        }
+
+        public CardSet SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                SetProperty(ref _selectedItem, value);
+                OnItemSelected(value);
+            }
+        }
+
+        private async void OnAddItem(object obj)
+        {
+            await Shell.Current.GoToAsync(nameof(NewCardSetPage));
+        }
+
+        async void OnItemSelected(CardSet cardSet)
+        {
+            if (cardSet == null)
+                return;
+
+            // This will push the ItemDetailPage onto the navigation stack
+            await Shell.Current.GoToAsync($"{nameof(CardSetDetailPage)}?{nameof(CardSetDetailViewModel.ItemId)}={cardSet.Id}");
+        }
+    }
+}
