edbaa9e403887e8f5919f0ee219bb69a234d1376
stillProger <killumexxx@gmail.com>
2021-12-08 18:46:08 +0400

    feat: Added new model: FileImage

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Models/FileImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FlashcardsCourseProject.Models
+{
+    public class FileImage
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public List<Card> Cards { get; set; } = new List<Card>();
+        public List<CardSet> CardSets { get; set; } = new List<CardSet>();
+
+    }
+}
