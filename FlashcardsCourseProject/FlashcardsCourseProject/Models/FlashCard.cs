904cff19961c5d570ad6bb5c627005c59a4a4132
stillProger <killumexxx@gmail.com>
2022-06-04 23:40:08 +0300

    refactor: rename models

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Models/FlashCard.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlashcardsCourseProject.Models
+{
+    public class FlashCard
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FrontSideText { get; set; }
+        public string FrontSideImagePath { get; set; }
+        public string BackSideImagePath { get; set; }
+        public string BackSideText { get; set; }
+
+        [ForeignKey("CardSetId")]
+        public int FlashCardSetId { get; set; } // Внешний ключ
+        public FlashCardSet FlashCardSet { get; set; } // Навигационное ствойство
+
+    }
+}
--- FlashcardsCourseProject/FlashcardsCourseProject/Models/FlashCardSet.cs
+++ FlashcardsCourseProject/FlashcardsCourseProject/Models/FlashCardSet.cs
@@ -4,16 +4,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FlashcardsCourseProject.Models
 {
-    public class CardSet
+    public class FlashCardSet
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
-        public string PicturePath { get; set; }
-        public string CreateDate { get; set; }
-        public string EditDate { get; set; }
-        public List<Card> Cards { get; set; } = new List<Card>();
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public List<FlashCard> Cards { get; set; } = new List<FlashCard>();
 
     }
 }
