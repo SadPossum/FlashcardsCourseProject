908f580872c9aafd635b046fe52d0c8fa3b782ed
stillProger <killumexxx@gmail.com>
2022-06-05 16:29:07 +0300

    feat: ListRequest model

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Services/Models/ListRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashcardsCourseProject.Services.Models
+{
+    public class ListRequest
+    {
+        public string Search { get; set; }
+        public Pagination Pagination { get; set; }
+    }
+}
