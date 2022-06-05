17a449ca290803e7a817a296a58cf190163f6a5e
stillProger <killumexxx@gmail.com>
2022-06-05 16:29:26 +0300

    feat: Pagination model

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Services/Models/Pagination.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashcardsCourseProject.Services.Models
+{
+    public class Pagination
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; }
+    }
+}
