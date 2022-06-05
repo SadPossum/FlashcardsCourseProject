0b0f17c08934eb20260cdfc3040a6b0f43fb074f
stillProger <killumexxx@gmail.com>
2022-06-05 16:35:44 +0300

    feat: Autharization data store

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Services/AutharizationDataStore.cs
@@ -0,0 +1,30 @@
+﻿using FlashcardsCourseProject.Models;
+using FlashcardsCourseProject.Services.API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FlashcardsCourseProject.Services
+{
+    public class AutharizationDataStore : IAutharizationDataStore
+    {
+        private RestAPIService _restAPIService => DependencyService.Get<RestAPIService>();
+
+        //TODO: Проверка, если авторизация прошла не успешно
+        public async Task<User> Autharization(string login, string password)
+        {
+            string postCardSetRequest = "/Autharization";
+            User newUser = await _restAPIService.Post<User>(postCardSetRequest,
+                new
+                {
+                    Login = login,
+                    Password = password,
+                });
+
+
+            return await Task.FromResult(newUser);
+        }
+    }
+}
---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Services/IAutharizationDataStore.cs
@@ -0,0 +1,13 @@
+﻿using FlashcardsCourseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashcardsCourseProject.Services
+{
+    public interface IAutharizationDataStore
+    {
+        Task<User> Autharization(string login, string password);
+    }
+}
