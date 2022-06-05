bfe429e74c5f609780fd02339b0662368175df80
stillProger <killumexxx@gmail.com>
2022-06-05 16:31:56 +0300

    feat: Add new data models

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashcardsCourseProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public List<FlashCardSet> FlashCardSet { get; set; } = new List<FlashCardSet>();
+    }
+}
---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Models/UserAddedFlashCardSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashcardsCourseProject.Models
+{
+    public class UserAddedFlashCardSet
+    {
+        public int FlashCardSetId { get; set; } // Внешний ключ
+        public FlashCardSet FlashCardSet { get; set; } // Навигационное ствойство
+
+        public int UserId { get; set; } // Внешний ключ
+        public User User { get; set; } // Навигационное ствойство
+    }
+}
