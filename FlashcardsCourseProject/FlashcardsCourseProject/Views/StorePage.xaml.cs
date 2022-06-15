18f1292137d36da450c7b7e626086fb5968346b5
stillProger <killumexxx@gmail.com>
2022-06-15 22:01:22 +0300

    store

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/Views/StorePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FlashcardsCourseProject.ViewModels;
+
+using Xamarin.Forms;
+
+namespace FlashcardsCourseProject.Views
+{
+    public partial class StorePage : ContentPage
+    {
+        private readonly CardSetViewModel _cardSetViewModel;
+        public StorePage()
+        {
+            InitializeComponent();
+
+            BindingContext = _cardSetViewModel = new CardSetViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _cardSetViewModel.OnAppearing();
+        }
+    }
+}
