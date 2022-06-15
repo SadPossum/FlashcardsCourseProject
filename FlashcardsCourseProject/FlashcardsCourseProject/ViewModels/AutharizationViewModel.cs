0854da27bdcb176baa84785303bbafee7aa5fdf9
stillProger <killumexxx@gmail.com>
2022-06-15 22:11:54 +0300

    Create AutharizationViewModel.cs

---
+++ FlashcardsCourseProject/FlashcardsCourseProject/ViewModels/AutharizationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FlashcardsCourseProject.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FlashcardsCourseProject.ViewModels
+{
+    public class AutharizationViewModel
+    {
+        public Command AutharizationCommand { get; }
+
+        public AutharizationViewModel()
+        {
+            AutharizationCommand = new Command(Autharization);
+        }
+        private async void Autharization(object obj)
+        {
+            await Shell.Current.GoToAsync($"{nameof(AboutPage)}");
+        }
+    }
+}
